1551d8a4c55f4dd5238e84bad2488464bad1296a
Jennifer Jung <jenjung2194@gmail.com>
2020-11-28 21:28:30 +0000

    create myListings, login and search pages

--- server.js
+++ server.js
@@ -60,3 +60,20 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+
+app.get("/search", (req, res) => {
+
+  res.render("search");
+});
+
+
+app.get("/listings", (req, res) => {
+
+  res.render("listings");
+});
+
+app.get("/login", (req, res) => {
+
+  res.render("login");
+});
