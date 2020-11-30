5d253f9b3570620775b92caa2f527b6945af8850
Jennifer Jung <jenjung2194@gmail.com>
2020-11-30 19:47:29 +0000

    implement test query using the user_id = 5, modify my-listings.ejs to render the items for the specific user, modify serach-listings.ejs and my-listings.ejs to show wheter it's available or not depenidng on the items.active value

--- routes/my-listings.js
+++ routes/my-listings.js
@@ -3,7 +3,25 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("search")
+
+    const queryString = `
+      SELECT *
+      FROM items
+      WHERE user_id = 5
+    `;
+
+    db
+    .query(queryString)
+    .then(result => {
+      const items = result.rows;
+      const templateVars = { items: {} };
+      for (item of items) {
+        templateVars.items[item.id] = item;
+      }
+      return res.render('my-listings', templateVars);
+    })
+    .catch(err => console.log('Error: ', err.stack));
+
   });
   return router;
 };
