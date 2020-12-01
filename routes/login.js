0ad850b5c0fffae9ab2ad9c26d666834cb007e96
Jennifer Jung <jenjung2194@gmail.com>
2020-12-01 18:18:59 +0000

    add templateVars in Login page

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    const
+
     res.render("login");
   });
   const getUserwithEmail = function (email) {
