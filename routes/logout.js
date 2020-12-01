d6d634b41893320809f2d862a30824b21cb5a49e
Jennifer Jung <jenjung2194@gmail.com>
2020-12-01 18:54:13 +0000

    fix the html structure to have the body tag include the header tag inside with the _header file

--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,8 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/", (req, res) => {
-    res.send("logout ok!")
+    req.session = null;
+    res.redirect("/");
   });
   return router;
 };
